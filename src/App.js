5965569836b04b053ca513e82258d82ba1a25881
NicoleLC16 <asalwaysnicole@outlook.com>
2020-02-21 09:36:03 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ButtonExampleButton from './ButtonExampleButton';
+import LoginForm from './components/LoginForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hi</h1>
+        <ButtonExampleButton />
+        <button class="ui button" role="button">Btn with Semantic-UI</button>
+        <LoginForm />
+      </div>
+    );
+  }
 }
 
 export default App;
