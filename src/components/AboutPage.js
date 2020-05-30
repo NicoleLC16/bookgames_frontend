101214f8be44d26073f260ba071ef7837b68d487
NicoleLC16 <asalwaysnicole@outlook.com>
2020-05-29 21:26:55 -0700

    fixing headers about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -15,7 +15,7 @@ class GamesContainer extends Component {
       <ul>
 
         <li>
-          Responsibilities As A Host:
+          <i><b>Responsibilities As A Host:</b></i>
           <ul>
             <li>Create a game that determines the tasks that players need to complete for each block on the board</li>
             <li>Make the rules for your own game. Hosts can determine the points system for their own games.</li>
@@ -25,9 +25,9 @@ class GamesContainer extends Component {
           </ul> 
 
         <li>
-          Tips for Hosting a Game:
+          <i><b>Tips for Hosting a Game:</b></i>
           <ul>
-            <li>Rules</li>
+            <li><i>Rule Making</i></li>
               <ul>
                 <li>Make certain rules for the bingo portion of the game.
                   Will the game give points 5 in a row completion? 
@@ -40,7 +40,7 @@ class GamesContainer extends Component {
                   1 point for each 100pgs(capping this at 500pgs is also a good idea),
                   Half points should be considered for picture books</li>
               </ul>
-            <li>Task Ideas</li>
+              <li><i>Task Ideas</i></li>
               <ul>
                 <li>Read with a book that starts with......(any letter of your choice)</li>
                 <li>Read a book by...(certain author)</li>
